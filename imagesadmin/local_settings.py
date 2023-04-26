1ac99f3f5e21e4f7ab41d5c083eb12725defe282
Alberto Hernandez Santos <hernandezsalberto@gmail.com>
2023-04-26 00:21:53 -0600

    Se Añade a settings.py

---
+++ imagesadmin/local_settings.py
@@ -0,0 +1,16 @@
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+import os
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ['DATABASE_ENGINE'],
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': os.environ['DATABASE_PORT']
+    }
+}
