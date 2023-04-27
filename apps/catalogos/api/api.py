08f22e74ee9ee3173f06bb53d223f58cfd70f3ec
Alberto Hernandez Santos <hernandezsalberto@gmail.com>
2023-04-27 00:10:57 -0600

    Probando serializadores

--- apps/catalogos/api/api.py
+++ apps/catalogos/api/api.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from .serializers import UbicacionSerializer
 
 from apps.catalogos.models import Ubicacion
@@ -10,9 +11,28 @@ class UbicacionViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     serializer_class = UbicacionSerializer
     
+# class ZoneSerializer(ModelSerializer):
+#     parent = SerializerMethodField()
+
+#     class Meta:
+#         model = Zone
+#         fields = ('id', 'name', 'project', 'parent',)
+
+#     def get_parent(self, obj):
+#         if obj.parent is not None:
+#             return ZoneSerializer(obj.parent).data
+#         else:
+#             return None
 class UbicacionAPIVIEW(APIView):
     
     def get(self, request):
         ubicaciones = Ubicacion.objects.all()
         ubicacion_serializer = UbicacionSerializer(ubicaciones, many=True)
         return Response(ubicacion_serializer.data)
+    
+# @api_view
+# def ubicacion_api_view(request):
+#     if request.method == 'GET':
+#         ubicaciones = Ubicacion.objects.all()
+#         ubicacion_serializer = UbicacionSerializer(ubicaciones, many=True)
+#         return Response(ubicacion_serializer.data)
--- apps/catalogos/api/serializers.py
+++ apps/catalogos/api/serializers.py
@@ -5,5 +5,6 @@ class UbicacionSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Ubicacion
-        fields = ('id','nombre','estatus','created_at')
-        read_only_fields = ('created_at', )
+        fields = '__all__'
+        # fields = ('id','nombre','estatus','created_at')
+        # read_only_fields = ('created_at', )
